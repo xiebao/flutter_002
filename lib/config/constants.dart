5f9ec4388aa58104306544c4e3f00d99c4badd8e
vicent <vicent0214@qq.com>
2019-04-28 17:33:41 +0800

    调整全局样式

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static const String IconFontFamily = "IconFont";
-  static const Color BackgroundColor = Colors.black12;
+  static const Color BackgroundColor = Color.fromRGBO(240, 240, 240, 1);
   static const Color BorderColor = Colors.black12;
 }
